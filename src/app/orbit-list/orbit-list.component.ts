41a84c33d9a0ebd8fb9d0f938c10b16295b2b124
rsunithat <rsunithat@gmail.com>
2020-12-13 12:39:05 -0600

    updated to step 4

---
+++ src/app/orbit-list/orbit-list.component.ts
@@ -0,0 +1,18 @@
+import { Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Satellite } from '../satellite';
+@Component({
+  selector: 'app-orbit-list',
+  templateUrl: './orbit-list.component.html',
+  styleUrls: ['./orbit-list.component.css']
+})
+export class OrbitListComponent implements OnInit {
+
+  constructor() { }
+  @Input() satellites: Satellite[];    
+    
+
+  ngOnInit() {
+  }
+ 
+}
